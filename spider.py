c0e5f01f343d88f10af029510990ac803d09e55f
xswift3 <13210240113@fudan.edu.cn>
2015-07-31 16:28:23 +0800

    spider.py modifition

--- spider.py
+++ spider.py
@@ -1,10 +1,11 @@
 #__author__ = 'v-waxun'
 #coding=utf-8
-import urllib
+import urllib2
 import codecs
 import re
 import time
 import os
+import sys
 def getFromHtml(html):
       #print html
      # f_0 = open('file1.html','w')
@@ -31,19 +32,18 @@ def getFromHtml(html):
       return urls,title
 
 def getHtml(url):
-       page = urllib.urlopen(url)
+       page = urllib2.urlopen(url,timeout=5)
        html = page.read()
        return html
 def isImageUrl(url):
-       if url.endswith(".com") or url.endswith(".asp") :
-           return False
-       imgReg = "^.*\.\w{3}$"
+       imgReg =  ".*(svg|bmp|gif|jpeg|png|jpg|css|js)$"
        regC = re.compile(imgReg)
        isMatch = regC.match(url)
-       if isMatch == None:
-           return False
-       else:
+       if isMatch != None:
            return True
+       else:
+           return False
+
 def isCssUrl(url):
     return url.find('css') != -1
 #url = "http://g1.dfcfw.com/g1/special/Apple.ico"
@@ -67,24 +67,28 @@ class Spider():
         self.m_id2urlMap = {}  
     
     def scrawler(self,url,depth):
+        if depth > self.m_maxDepth:
+            return 
+
         if url in self.m_url2idMap.keys():
             return
         else:
+            self.m_urlId += 1
             self.m_url2idMap[url] = self.m_urlId
             self.m_id2urlMap[self.m_urlId] = url
-
         print "visting url:" + url
         startTime = time.clock()
-        if depth > self.m_maxDepth:
-            return 
-      
-        titleName = DSTDIR + str(self.outFileCount)+'.title'
-        htmlName = DSTDIR + str(self.outFileCount) + ".html"
-        self.outFileCount += 1
-        html = getHtml(url)
+        
+        titleName = DSTDIR + str(self.m_urlId)+'.title'
+        htmlName = DSTDIR + str(self.m_urlId) + ".html"
+        try:  #remote server close the connection
+            html = getHtml(url)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            return
         fileHtml = open(htmlName,'w')
         fileHtml.write(html)
-    
+        
         urls,title = getFromHtml(html)
         print "visited %s time:%d"%(url,time.clock()-startTime)
     
@@ -95,15 +99,36 @@ class Spider():
             self.scrawler(u,depth + 1)
     def start(self):
         self.scrawler(self.m_startUrl,1)
+    def doSerialize(self):
+        serialFilePath = DSTDIR + "id2url.map"
+        mapFile = open(serialFilePath,'w')
+        for k in self.m_id2urlMap.keys():
+            line = "%d\t%s\n"%(k,self.m_id2urlMap[k])
+            mapFile.write(line)
+        mapFile.close()
+        print 'serilization done in id2url.map'
+             
+def printUsage():
+    print '''
+    usage:  python spider.py startUrl maxDepth
+    example:python spider.py http://www.sina.com 3
+    '''
 if __name__ == "__main__":
     dstPath = os.getcwd() + "\\" + SPIDER_OUT
     if os.path.exists(dstPath):
         os.remove(dstPath)
     os.mkdir(dstPath)
-
-    startUrl = "http://www.eastmoney.com/"
+    if len(sys.argv) != 3 :
+        printUsage()
+        sys.exit()
+    startUrl = sys.argv[1]
+    maxDepth = int(sys.argv[2])
+    print startUrl,maxDepth
+    #startUrl = "http://www.eastmoney.com/"
     #startUrl = "http://www.baidu.com/"
     #startUrl = "http://www.sina.com/"
-    spider = Spider(startUrl,3)
+    #maxDepth = 2
+    spider = Spider(startUrl,maxDepth)
     spider.start()
+    spider.doSerialize()
     #to do serilize maps ...
