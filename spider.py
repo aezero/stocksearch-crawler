a66a488da58f8664e5ef9548ac26e08f0262dca4
yinguangxue <yinguangxue@gmail.com>
2015-08-14 01:18:24 +0800

    multi_thread

--- spider.py
+++ spider.py
@@ -1,4 +1,5 @@
 #__author__ = 'v-waxun'
+#__modify__ = 'v-guayin'
 #coding=utf-8
 import urllib2
 import codecs
@@ -6,6 +7,12 @@ import re
 import time
 import os
 import sys
+import Queue
+import thread
+import threading
+import signal
+from tld import get_tld
+
 def getFromHtml(html):
       #print html
      # f_0 = open('file1.html','w')
@@ -15,7 +22,7 @@ def getFromHtml(html):
       if len(titleList) > 0:
           title = titleList[0]     
       for t in titleList:
-          print t
+          print(t)
           #u = t.decode('utf-8')
           #print u
       urlList = re.findall(urlC,html)
@@ -23,7 +30,7 @@ def getFromHtml(html):
       urls = []
       for url in urlList:
           if isImageUrl(url) or isCssUrl(url):
-              print "filter:" + url
+              print("filter:" + url)
           else:
               urls.append(url)
              #urllib.urlretrieve(imgurl,'%s.jpg' % x)
@@ -58,47 +65,81 @@ SPIDER_OUT = 'SpiderOut'
 DSTDIR = os.getcwd() + "\\" + SPIDER_OUT + "\\" 
 
 class Spider():
-    def __init__(self,startUrl,maxDepth):
+    def __init__(self,startUrl,maxPage):
         self.m_startUrl = startUrl
-        self.m_maxDepth = maxDepth
+        self.m_maxPage = maxPage
         self.outFileCount = 0
         self.m_urlId= 0
         self.m_url2idMap = {}
         self.m_id2urlMap = {}  
-    
-    def scrawler(self,url,depth):
-        if depth > self.m_maxDepth:
-            return 
-
-        if url in self.m_url2idMap.keys():
-            return
-        else:
-            self.m_urlId += 1
-            self.m_url2idMap[url] = self.m_urlId
-            self.m_id2urlMap[self.m_urlId] = url
-        print "visting url:" + url
-        startTime = time.clock()
+        self.lock = threading.Lock()
+        self.myqueue = Queue.Queue()
+        self.is_sigint_up = False
         
-        titleName = DSTDIR + str(self.m_urlId)+'.title'
-        htmlName = DSTDIR + str(self.m_urlId) + ".html"
-        try:  #remote server close the connection
-            html = getHtml(url)
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-            return
-        fileHtml = open(htmlName,'w')
-        fileHtml.write(html)
+    def scrawler2(self):
         
-        urls,title = getFromHtml(html)
-        print "visited %s time:%d"%(url,time.clock()-startTime)
-    
-        urlFile = open(titleName,'w')
-        urlFile.write(title)
+        while(not self.is_sigint_up):        
+            url = self.myqueue.get()
+            titleName = DSTDIR + str(self.m_url2idMap[url]) + '.title'
+            htmlName = DSTDIR + str(self.m_url2idMap[url]) + '.html'
+            html = ""
+			
+            try:
+                html = getHtml(url)
+                #print "###",html,"@@@"
+            except:
+                continue
+            
+            fileHtml = open(htmlName,'w')
+            fileHtml.write(html)
+            fileHtml.close()
+            
+            urls,title = getFromHtml(html)
+            
+            urlFile = open(titleName,'w')
+            urlFile.write(title)
+            urlFile.close()
+            
+            self.lock.acquire()
+            
+            for u in urls:
+                if u not in self.m_url2idMap:
+                    try:
+                        if get_tld(u) == get_tld(self.m_startUrl):
+                            self.m_urlId += 1
+                            self.m_id2urlMap[self.m_urlId] = u
+                            self.m_url2idMap[u] = self.m_urlId
+                            self.myqueue.put(u)
+                    except:
+                        pass
+            self.lock.release()
+        
+    def sigint_handler(self, signum, frame):
+        self.is_sigint_up = True
+        self.doSerialize()
+        
+    def start(self, thread_num):
+        signal.signal(signal.SIGINT, self.sigint_handler)
+        #self.scrawler(self.m_startUrl,1)
+        self.myqueue.put(self.m_startUrl)
+        self.m_urlId = 1
+        self.m_id2urlMap[1] = self.m_startUrl
+        self.m_url2idMap[self.m_startUrl] = 1
+        threads = []
+        for i in range(thread_num):
+            t = threading.Thread(target = self.scrawler2,args=())
+            t.setDaemon(True)
+            threads.append(t)
+            t.start()
+        
+        while True:
+            alive = False
+            for thread in threads:
+                alive = alive or thread.isAlive()
+            if not alive:
+                break
 
-        for u in urls:
-            self.scrawler(u,depth + 1)
-    def start(self):
-        self.scrawler(self.m_startUrl,1)
+        
     def doSerialize(self):
         serialFilePath = DSTDIR + "id2url.map"
         mapFile = open(serialFilePath,'w')
@@ -106,29 +147,27 @@ class Spider():
             line = "%d\t%s\n"%(k,self.m_id2urlMap[k])
             mapFile.write(line)
         mapFile.close()
-        print 'serilization done in id2url.map'
+        print('serilization done in id2url.map')
              
 def printUsage():
-    print '''
-    usage:  python spider.py startUrl maxDepth
+    print('''
+    usage:  python spider.py startUrl maxPage
     example:python spider.py http://www.sina.com 3
-    '''
+    ''')
 if __name__ == "__main__":
     dstPath = os.getcwd() + "\\" + SPIDER_OUT
-    if os.path.exists(dstPath):
-        os.remove(dstPath)
-    os.mkdir(dstPath)
+    if not os.path.exists(dstPath):
+        os.mkdir(dstPath)
     if len(sys.argv) != 3 :
         printUsage()
         sys.exit()
     startUrl = sys.argv[1]
-    maxDepth = int(sys.argv[2])
-    print startUrl,maxDepth
+    maxPage = int(sys.argv[2])
+    print(startUrl,maxPage)
     #startUrl = "http://www.eastmoney.com/"
     #startUrl = "http://www.baidu.com/"
     #startUrl = "http://www.sina.com/"
     #maxDepth = 2
-    spider = Spider(startUrl,maxDepth)
-    spider.start()
-    spider.doSerialize()
+    spider = Spider(startUrl,maxPage)
+    spider.start(maxPage)
     #to do serilize maps ...
